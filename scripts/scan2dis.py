a686655d209a902319033b569dc6ecd675f0069e
yoshiko-kulala <kulala.mbili@gmail.com>
2022-07-22 15:48:50 +0900

    add range front

--- scripts/scan2dis.py
+++ scripts/scan2dis.py
@@ -25,6 +25,14 @@ def scan_callback(msg):
     range_ahead6 = msg.ranges[(320*2)]
     range_ahead7 = msg.ranges[(35*2)]
     range_ahead8 = msg.ranges[(325*2)]
+
+    range_front1 = msg.ranges[(0)]
+    range_front2 = msg.ranges[(1*2)]*math.cos(math.radians(1))
+    range_front3 = msg.ranges[(2*2)]*math.cos(math.radians(2))
+    range_front4 = msg.ranges[(3*2)]*math.cos(math.radians(3))
+    range_front5 = msg.ranges[(359*2)]*math.cos(math.radians(1))
+    range_front6 = msg.ranges[(358*2)]*math.cos(math.radians(2))
+    range_front7 = msg.ranges[(357*2)]*math.cos(math.radians(3))
     
     cos25 = math.cos(math.radians(25))
     cos30 = math.cos(math.radians(30))
@@ -83,7 +91,24 @@ def scan_callback(msg):
     range_back=(range_back1+range_back2+range_back3+range_back4+range_back5+range_back6)/count_back
     range_ahead=(range_ahead1+range_ahead2+range_ahead3+range_ahead4+range_ahead5+range_ahead6+range_ahead7+range_ahead8)/count
 			
+    front_cou=0
+    if range_front1>0:
+        front_cou=front_cou+1
+    if range_front2>0:
+        front_cou=front_cou+1
+    if range_front3>0:
+        front_cou=front_cou+1
+    if range_front4>0:
+        front_cou=front_cou+1
+    if range_front5>0:
+        front_cou=front_cou+1
+    if range_front6>0:
+        front_cou=front_cou+1
+    if range_front7>0:
+        front_cou=front_cou+1
     
+    range_front = (range_front1 + range_front2 + range_front3 + range_front4 + range_front5 + range_front6 + range_front7)/front_cou
+
     print(range_ahead)
     print(range_back)
 
@@ -95,12 +120,15 @@ def scan_callback(msg):
         pub3.publish(range_left)
     if range_back>0:
         pub4.publish(range_back)
+    if range_front>0:
+        pub4.publish(range_front)
 
 
 pub1 = rospy.Publisher('range_right', Float32, queue_size=10)
 pub2 = rospy.Publisher('range_ahead', Float32, queue_size=10)
 pub3 = rospy.Publisher('range_left', Float32, queue_size=10)
 pub4 = rospy.Publisher('range_back', Float32, queue_size=10)
+pub5 = rospy.Publisher('range_front', Float32, queue_size=10)
 rospy.init_node('range_ahead')
 
 scan_sub = rospy.Subscriber('scan', LaserScan, scan_callback)
